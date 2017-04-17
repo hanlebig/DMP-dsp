9dd45a5055914795c54fa5bb0063f2f43fd1a828
18335196765@163.com <18335196765@163.com>
2017-04-17 13:41:00 +0800

    短链接

--- down/dsp/src/main/java/com/taiyear/dsp/utils/RemoteSendMessage.java
+++ down/dsp/src/main/java/com/taiyear/dsp/utils/RemoteSendMessage.java
@@ -1,21 +1,42 @@
 package com.taiyear.dsp.utils;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+
+import com.alibaba.fastjson.JSON;
 
 
 public class RemoteSendMessage {
 	public static final String USERID = "184";
 	public static final String ACCOUNT = "taiyetg";
 	public static final String PAASSWORD = "888888";
-	
+	public static void main(String[] args) {
+		Map<String, String> data = new HashMap<String, String>();
+		String url = "http://blog.csdn.net/czp11210/article/details/47022639";
+		data.put("url", url);
+//		data.put("signature", "su2dn2nx3s2f4e12ewdcx4dx3xe3xsd3f");
+		data.put("app_id", "100001");
+		String d = JSON.toJSONString(data);
+		List<String> list = new ArrayList<String>();
+		for (int j = 0; j < d.length(); j++) {
+			list.add(String.valueOf(d.charAt(j)));
+		} 
+//		System.out.println(Arrays.sort(list));	
+		//		String url = RemoteSendMessage.generateShortUrlSo("http://blog.csdn.net/czp11210/article/details/47022639");
+//		System.out.println(url);
+	}
 	public static String generateShortUrlSo(String url){
 		Map<String, String> data = new HashMap<String, String>();
 		data.put("url", url);
+		data.put("signature", "su2dn2nx3s2f4e12ewdcx4dx3xe3xsd3f");
+		data.put("app_id", "100001");
 		try {
 			return Jsoup.connect("http://bil.vc/shortUrl.php").data(data).post().text();
 		} catch (IOException e) {
